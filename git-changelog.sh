d5547ff8e8963999faa37e032edac8e9e23d1d94
picas <me@picas.fr>
2016-06-23 08:15:29 +0200

    cleanup of functions + new remotes and branches script options

--- git-changelog.sh
+++ git-changelog.sh
@@ -19,34 +19,36 @@
 #       git-changelog.sh all
 #       git-changelog.sh --file=CHANGELOG.md ...
 #
-
-# abort script at first command with a non-zero status
 set -e
-## for dev usage: debug commands before to execute them
-#set -x
-## for dev usage: print shell input lines as they are read
-#set -v
 
-# current version
+# command info
 declare -r SCRIPT_VERSION='0.1.0'
 declare -r SCRIPT_NAME='GIT-changelog'
 declare -r SCRIPT_SOURCES='http://github.com/e-picas/git-changelog'
 declare -r SCRIPT_LICENSE='MIT license'
 declare -r CMD_PROG="$(basename "$0")"
 declare -r short_opts='qvx'
-declare -r long_opts='help,version,quiet,verbose,debug,file:,mask-title:,mask-header-upcoming:,mask-header-tag:,mask-commit:,tag-ignore:'
+declare -r long_opts='help,version,quiet,verbose,debug,set-opt-v,set-opt-x,\
+    file:,mask-title:,mask-header-upcoming:,mask-header-tag:,mask-commit:,tag-ignore:,\
+    origin:,master:,remotes:';
 
+# environment flags
 declare DEBUG=false
 declare QUIET=false
 declare VERBOSE=false
-declare TARGET_FILENAME=''
 
 # variables
+declare TARGET_FILENAME=''
 declare TAGNAME_IGNORE='#no-changelog'
 declare MASK_GLOBAL_TITLE='# Change log for remote repository <%s>'
 declare MASK_HEADER_UPCOMING='* (upcoming release)'
 declare MASK_HEADER_TAG='* %(tag) (%(taggerdate:short) - %%s)'
 declare MASK_COMMIT_LOG='    * %h - %s (@%cN)'
+
+# repository organization
+declare ORIGIN_REMOTE='origin'
+declare MASTER_BRANCH='master'
+declare ALTERNATE_REMOTES='upstream'
 
 # throw an error
 error() {
@@ -59,12 +61,12 @@ error() {
 
 # echo to SDTERR in VERBOSE mode
 verbose_echo() {
-    $VERBOSE && echo "$*" >&2;
+    $VERBOSE && echo "$*" >&2 || return 0;
 }
 
 # echo to SDTERR in DEBUG mode
 debug_echo() {
-    $DEBUG && echo "$*" >&2;
+    $DEBUG && echo "$*" >&2 || return 0;
 }
 
 # usage string
@@ -73,6 +75,7 @@ usage() {
 usage: $CMD_PROG [--help] [--version] [-v | --verbose] [-q | --quiet]
             [--file=<path>] [--mask-title=<mask>] [--tag-ignore=<tagname>]
             [--mask-header-upcoming=<mask>] [--mask-header-tag=<mask>] [--mask-commit=<mask>]
+            [--origin=<name>] [--master=<name>] [--remotes=<coma,list>]
             <type>
 MESSAGE
 }
@@ -90,6 +93,7 @@ help() {
     cat <<MESSAGE
 
 with <type> in:
+        'list-tags'                     get a list of all available tags like '<tag_name>  <tag_hash>  <tag_remote>'
         'all'                           get the full repository changes log
         'init'                          get the full repository initial changes log (no tag is used)
         'tag1..tag2'                    get the changes log between tag1 and tag2 (tag1 < tag2)
@@ -97,12 +101,28 @@ with <type> in:
 
 available options:
         --file=<path>                   write the result in a file
-        --tag-ignore=<tagname>          tag to match ignored commits ; default is: '$TAGNAME_IGNORE'
-        --mask-title=<mask>             mask used to build global title ; default is: '$MASK_GLOBAL_TITLE'
-        --mask-header-upcoming=<mask>   mask used to build upcoming release title; default is: '$MASK_HEADER_UPCOMING'
-        --mask-header-tag=<mask>        mask used to build tag title ; default is: '$MASK_HEADER_TAG'
+        --help                          get this help information
         --mask-commit=<mask>            mask used to build commit message ; default is: '$MASK_COMMIT_LOG'
+        --mask-header-tag=<mask>        mask used to build tag title ; default is: '$MASK_HEADER_TAG'
+        --mask-header-upcoming=<mask>   mask used to build upcoming release title; default is: '$MASK_HEADER_UPCOMING'
+        --mask-title=<mask>             mask used to build global title ; default is: '$MASK_GLOBAL_TITLE'
+        --master=<name>                 name of the 'master' branch ; default is: '$MASTER_BRANCH'
+        --origin=<name>                 name of the 'origin' remote ; default is: '$ORIGIN_REMOTE'
+        -q, --quiet                     decrease script's verbosity
+        --remotes=<coma,list>           coma separated list of allowed remotes ; default is: '$ALTERNATE_REMOTES'
+        --tag-ignore=<tagname>          tag to match ignored commits ; default is: '$TAGNAME_IGNORE'
+        -v, --verbose                   increase script's verbosity
+        --version                       get current command version
 MESSAGE
+    if $DEBUG; then
+        cat <<MESSAGE
+
+development options:
+        -x, --debug                     enable the DEBUG variable
+        --set-opt-x                     enable 'set -x' (internal option): debug commands before to execute them
+        --set-opt-v                     enable 'set -v' (internal option): print shell input lines as they are read
+MESSAGE
+    fi
     echo
     echo "This is free software under the terms of the ${SCRIPT_LICENSE}."
     echo "See <${SCRIPT_SOURCES}> for sources & updates."
@@ -113,25 +133,35 @@ trim() {
     echo -e "${1}" | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//';
 }
 
-# repo_remote
-repo_remote() {
+# get first line of a multiline variable
+first_line() {
+    local arg=( "$*" )
+    echo "${arg[0]}"
+}
+
+# usage: var_name=( "$(coma_list_to_array <list>)" )
+coma_list_to_array() {
+    echo "$*" | tr ',' '\n';
+}
+
+coma_list_to_regexp() {
+    echo "$*" | tr ',' '|';
+}
+
+# get_remote_url
+get_remote_url() {
     echo "$(git remote show -n origin | grep 'Fetch' | cut -d':' -f 2-)"
 }
 
-# tag_title TAG_REF
-tag_title() {
-    local TAGREF=$(tag_header "${1}")
+# build_tag_title TAG_REF
+build_tag_title() {
+    local TAGREF=$(get_tag_hash "${1}")
     local tmp=$(git --no-pager for-each-ref --sort='-taggerdate' --format="$MASK_HEADER_TAG" 'refs/tags' | grep " ${1} ")
     printf "$tmp" "$TAGREF"
 }
 
-# tag_header TAG_REF
-tag_header() {
-    git --no-pager show-ref --hash --abbrev "${1}"
-}
-
-# tag_history TAG1 TAG2
-tag_history() {
+# get_history_tag TAG1 TAG2
+get_history_tag() {
     local history_tmp=''
     if [ $# -eq 2 ]; then
         history_tmp="$(git --no-pager log --oneline --first-parent --no-merges --decorate --pretty=tformat:"$MASK_COMMIT_LOG" "${1}..${2}")"
@@ -149,8 +179,8 @@ tag_history() {
     echo "$history_tmp"
 }
 
-# commit_history HASH
-commit_history() {
+# get_history_commit HASH
+get_history_commit() {
     local history_tmp="$(git --no-pager log --oneline --first-parent --no-merges --decorate --pretty=tformat:"$MASK_COMMIT_LOG" -1 "${1}")"
     if [ -n "$TAGNAME_IGNORE" ]; then
         local res="$(echo "$history_tmp" | grep -v "$TAGNAME_IGNORE" &>/dev/null; echo $?)"
@@ -163,8 +193,8 @@ commit_history() {
     echo "$history_tmp"
 }
 
-# get_history
-get_history() {
+# get_history_full
+get_history_full() {
     local history_tmp="$(git --no-pager log --oneline --all --decorate --pretty=tformat:"$MASK_COMMIT_LOG")"
     if [ -n "$TAGNAME_IGNORE" ]; then
         local res="$(echo "$history_tmp" | grep -v "$TAGNAME_IGNORE" &>/dev/null; echo $?)"
@@ -177,7 +207,64 @@ get_history() {
     echo "$history_tmp"
 }
 
-# get_changelog TAG1 TAG2
+# get the last commit hash on master branch
+get_last_commit_hash() {
+    git rev-parse "$MASTER_BRANCH"
+}
+
+# get a tag commit hash
+get_tag_hash() {
+    local tagname="$1"
+    if [ -e ".git/refs/tags/${tagname}" ]; then
+#        git for-each-ref --format='%(objectname)' "refs/tags/${tagname}"
+        git --no-pager show-ref --hash --abbrev "${tagname}"
+    fi
+}
+
+# get the remotes containing a commit hash
+get_remotes_from_hash() {
+    local hash="$1"
+    git branch -r --contains "$hash"
+}
+
+# guess original remote of a tag
+guess_tag_original_remote() {
+    local tagname="$1"
+    tag_hash="$(get_tag_hash "$tag")"
+    tag_remote=''
+    tag_remote_full="$(get_remotes_from_hash "$tag_hash")"
+    tag_remote_tmp="$(echo "$tag_remote_full" | grep -v "${ORIGIN_REMOTE}/")"
+
+    if [ $? -eq 0 ]; then
+        tag_remote_tmp="$(echo "$tag_remote_tmp" | grep -v "${ORIGIN_REMOTE}/")"
+        remotes_grep="$(coma_list_to_regexp "$ALTERNATE_REMOTES")"
+        if [ "$(echo "$tag_remote_tmp" | grep -E "(${remotes_grep})/" &>/dev/null; echo $?)" -eq 0  ]; then
+            if (( $(grep -c . <<<"$tag_remote_tmp") > 1 )); then
+                if [ "$(echo "$tag_remote_tmp" | grep "/${MASTER_BRANCH}" &>/dev/null; echo $?)" -eq 0  ]; then
+                    tag_remote="$(echo "$tag_remote_tmp" | grep "/${MASTER_BRANCH}")"
+                else
+                    tag_remote="$(first_line "$tag_remote_tmp")"
+                fi
+            else
+                tag_remote="$tag_remote_tmp"
+            fi
+        else
+            if [ "$(echo "$tag_remote_tmp" | grep "/${MASTER_BRANCH}" &>/dev/null; echo $?)" -eq 0  ]; then
+                tag_remote="$(echo "$tag_remote_tmp" | grep "/${MASTER_BRANCH}")"
+            else
+                tag_remote="$(first_line "$tag_remote_tmp")"
+            fi
+        fi
+    else
+        tag_remote="$(first_line "$(echo "$tag_remote_full" | grep -v ' -> ')")"
+    fi
+
+    if [ -n "$tag_remote" ]; then
+        echo "$(trim "$tag_remote")"
+    fi
+}
+
+# get_changelog ref1 [ref2]
 get_changelog() {
     if [ $# -eq 2 ]; then
         local TAG1="${1}"
@@ -191,13 +278,13 @@ get_changelog() {
     if [ "$TAG2" = 'HEAD' ]; then
         echo "$MASK_HEADER_UPCOMING"
     else
-        echo "$(tag_title "$TAG2")"
+        echo "$(build_tag_title "$TAG2")"
     fi
     echo
     if [ -n "$TAG1" ]; then
-        tag_history "$TAG1" "$TAG2"
+        get_history_tag "$TAG1" "$TAG2"
     else
-        tag_history "$TAG2"
+        get_history_tag "$TAG2"
     fi
     echo
 }
@@ -205,24 +292,24 @@ get_changelog() {
 # action 'init'
 action_init() {
     verbose_echo "> calling action 'init'"
-    REPO=$(repo_remote)
+    REPO=$(get_remote_url)
     if [ -n "$TARGET_FILENAME" ]; then
         {   echo "$(printf "$MASK_GLOBAL_TITLE" "$(trim "$REPO")")"
             echo
-            get_history
+            get_history_full
         } > "$TARGET_FILENAME"
         $EDITOR "$TARGET_FILENAME"
     else
         echo "$(printf "$MASK_GLOBAL_TITLE" "$(trim "$REPO")")"
         echo
-        get_history
+        get_history_full
     fi
 }
 
 # action 'full' or 'all'
 action_all() {
     verbose_echo "> calling action 'all'"
-    REPO=$(repo_remote)
+    REPO=$(get_remote_url)
     if [ -n "$TARGET_FILENAME" ]; then
         {   echo "$(printf "$MASK_GLOBAL_TITLE" "$(trim "$REPO")")"
             echo
@@ -267,13 +354,26 @@ action_all() {
     fi
 }
 
-dev_debug() {
+# action 'list-tags'
+action_list_tags() {
+    for tag in $(git tag --sort='-taggerdate'); do
+        tag_remote="$(guess_tag_original_remote "tag")"
+        tag_hash="$(get_tag_hash "$tag")"
+        echo "$tag $tag_hash $tag_remote"
+    done
+}
+
+# special debug action (for development)
+action_debug_env() {
     version
     echo
     echo "## command debug"
     echo "DEBUG                 : $DEBUG"
     echo "QUIET                 : $QUIET"
     echo "VERBOSE               : $VERBOSE"
+    echo "ORIGIN_REMOTE         : '$ORIGIN_REMOTE'"
+    echo "MASTER_BRANCH         : '$MASTER_BRANCH'"
+    echo "ALTERNATE_REMOTES     : '$ALTERNATE_REMOTES'"
     echo "TARGET_FILENAME       : '$TARGET_FILENAME'"
     echo "TAGNAME_IGNORE        : '$TAGNAME_IGNORE'"
     echo "MASK_GLOBAL_TITLE     : '$MASK_GLOBAL_TITLE'"
@@ -300,6 +400,14 @@ while [ $# -gt 0 ]; do
         -q | --quiet )      QUIET=true; VERBOSE=false;;
         -v | --verbose )    VERBOSE=true; QUIET=false;;
         -x | --debug )      DEBUG=true;;
+        --set-opt-x )
+            ## for dev usage: debug commands before to execute them
+            set -x
+            ;;
+        --set-opt-v )
+            ## for dev usage: print shell input lines as they are read
+            set -v
+            ;;
         --version )
             version; exit 0;;
         --help )
@@ -328,6 +436,18 @@ while [ $# -gt 0 ]; do
             TAGNAME_IGNORE="$(echo "$2" | cut -d'=' -f2)"
             verbose_echo "> tag to match ignored commits over-written as '$TAGNAME_IGNORE'"
             shift;;
+        --master )
+            MASTER_BRANCH="$(echo "$2" | cut -d'=' -f2)"
+            verbose_echo "> master branch over-written as '$MASTER_BRANCH'"
+            shift;;
+        --origin )
+            ORIGIN_REMOTE="$(echo "$2" | cut -d'=' -f2)"
+            verbose_echo "> origin remote over-written as '$ORIGIN_REMOTE'"
+            shift;;
+        --remotes )
+            ALTERNATE_REMOTES="$(echo "$2" | cut -d'=' -f2)"
+            verbose_echo "> allowed remotes over-written as '$ALTERNATE_REMOTES'"
+            shift;;
         -- )    shift; break;;
         * )     error "unknown option '$1'";;
     esac
@@ -350,7 +470,11 @@ ARG="$1"
 
 # special development action
 if [ "$ARG" = 'debug' ]; then
-    dev_debug;
+    action_debug_env;
+
+# get the list of all tags
+elif [ "$ARG" = 'list-tags' ]; then
+    action_list_tags;
 
 # get the whole repo history
 elif [ "$ARG" = 'full' ]||[ "$ARG" = 'all' ]; then
@@ -377,7 +501,7 @@ else
         # get the history of a single commit
         if [ "$commit" = 0 ]; then
             verbose_echo "> getting commit message"
-            commit_history "$ARG"
+            get_history_commit "$ARG"
 
         else
             # else error, argument not understood
